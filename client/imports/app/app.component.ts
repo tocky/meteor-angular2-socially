358dc977ed165aa38cbfc126ba729f6ac6fb6723
Shin Tokiwa <shin.tokiwa@zealot.co.jp>
2016-11-21 16:13:39 +0900

    Implement step 0
    https://www.angular-meteor.com/tutorials/socially/angular2/bootstrapping

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,13 +1,9 @@
-import { Component } from "@angular/core";
-import template from "./app.component.html";
-import style from "./app.component.scss";
+import { Component } from '@angular/core';
+
+import template from './app.component.html';
 
 @Component({
-  selector: "app",
-  template,
-  styles: [ style ]
+  selector: 'app',
+  template
 })
-export class AppComponent {
-  constructor() {
-  }
-}
+export class AppComponent {}
