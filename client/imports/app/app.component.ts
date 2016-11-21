9309d96fb94caa5d499e95302a3707c842d343d1
Shin Tokiwa <shin.tokiwa@zealot.co.jp>
2016-11-21 16:59:46 +0900

    Implement step 3
    https://www.angular-meteor.com/tutorials/socially/angular2/3-way-data-binding

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { Parties } from '../../../both/collections/parties.collection.ts';
+import { Party } from '../../../both/models/party.model';
 
 import template from './app.component.html';
 
@@ -7,25 +11,9 @@ import template from './app.component.html';
   template
 })
 export class AppComponent {
-  parties: any[];
+  parties: Observable<Party[]>;
 
   constructor() {
-    this.parties = [
-      {
-        'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to debstep.',
-        'location': 'Palo Alto'
-      },
-      {
-        'name': 'All dubstep all the time',
-        'description': 'Get it on!',
-        'location': 'Palo Alto'
-      },
-      {
-        'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.',
-        'location': 'San Francisco'
-      },
-    ];
+    this.parties = Parties.find({}).zone();
   }
 }
